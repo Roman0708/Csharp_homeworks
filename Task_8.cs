cd78f92e0534116a6a0be7b0b272f8583c46a875
Roman Kalimov <Roman.Kalimov@icl-services.com>
2022-09-12 00:05:22 +0300

    Added Task_8 and deleted Program.cs from git

---
+++ Task_8.cs
@@ -0,0 +1,9 @@
+﻿Console.WriteLine("Введите число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+int count = 2; // В задании сказано что нужно выводить все числа начиная с 1 - поэтому 0 и отрицательные числа в расчет не взял, надеюсь правильно
+while ( count <= a )
+{
+    Console.Write(count + ", ");
+    count = count + 2;
+}
