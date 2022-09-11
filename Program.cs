2bb16b7ac61aa8823cc7016a9c1691de4c527eef
Roman Kalimov <Roman.Kalimov@icl-services.com>
2022-09-11 22:31:55 +0300

    First variant

--- Program.cs
+++ Program.cs
@@ -1,2 +1,15 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿//Введение чисел по очереди использовано чтобы предотвратить присваивание чисел в одну переменную
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine("max = " + a);
+}
+else
+{
+    Console.WriteLine("max = " + b);
+}
