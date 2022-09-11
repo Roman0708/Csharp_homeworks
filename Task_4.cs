169d765b7c01a107ecd108e9aa9ded3ea2381c04
Roman Kalimov <Roman.Kalimov@icl-services.com>
2022-09-11 23:26:54 +0300

    Task 4 added to repository

---
+++ Task_4.cs
@@ -0,0 +1,25 @@
+﻿//Введение чисел по очереди использовано чтобы предотвратить присваивание чисел в одну переменную
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите третье число");
+int c = Convert.ToInt32(Console.ReadLine());
+
+if (a <= b)
+{
+    if (b <= c)
+    {
+        Console.WriteLine( c ); //В примере к задаче на вывод указано только число, поэтому никаких тесктовых пояснений не ставил
+    }
+    else
+    {
+        Console.WriteLine( b );
+    }
+}
+else
+{
+    Console.WriteLine( a );
+}
