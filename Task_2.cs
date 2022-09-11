4fe900de73908effad8422e743899769d080dade
Roman Kalimov <Roman.Kalimov@icl-services.com>
2022-09-11 23:44:34 +0300

    Renamed Program to Task_2

---
+++ Task_2.cs
@@ -0,0 +1,15 @@
+﻿//Введение чисел по очереди использовано чтобы предотвратить присваивание чисел в одну переменную
+Console.WriteLine("Введите первое число");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.WriteLine("Введите второе число");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine("max = " + a);
+}
+else
+{
+    Console.WriteLine("max = " + b);
+}
