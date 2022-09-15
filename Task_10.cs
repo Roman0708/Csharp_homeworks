c67a62cd23162e48887157f33926b413e3953d51
Roman Kalimov <Roman.Kalimov@icl-services.com>
2022-09-15 22:53:43 +0300

    First variant of Task_10

---
+++ Task_10.cs
@@ -0,0 +1,18 @@
+﻿//Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+//456 -> 5
+//782 -> 8
+//918 -> 1
+
+Console.WriteLine("Введите трёхзначное число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+if (num < 100 || num > 999)
+{
+    Console.WriteLine("Это не трёхзначное число"); //Не уверен нужно ли это  и можно ли так
+}
+else
+{
+    int FirstAndSecondFigure = num/10;
+    int SecondFigure = FirstAndSecondFigure%10;
+    Console.WriteLine(SecondFigure);
+}
