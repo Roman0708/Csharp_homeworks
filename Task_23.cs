581efc7721cea31c4cbd9db7beb38f0f6cc776c7
Roman Kalimov <Roman.Kalimov@icl-services.com>
2022-09-19 03:11:18 +0300

    Corrected variant

---
+++ Task_23.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Введите число");
+int num = Convert.ToInt32(Console.ReadLine());
+
+
+for ( int i = 1; i <= num; i++) Console.Write($","+Math.Pow(i, 3));
+
